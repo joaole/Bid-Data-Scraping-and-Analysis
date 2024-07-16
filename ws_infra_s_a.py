f3bd7be77282b7724646059ac61eea53959c30be
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-07-16 09:43:45 -0300

    Update ws_infra_s_a.py

--- ws_infra_s_a.py
+++ ws_infra_s_a.py
@@ -2,49 +2,50 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from save_to_excel import save_to_excel
+from remove_special_characters import remove_special_characters
+from keywords import keywords
+
+def keywords_filter(df, keywords):
+    pattern = '|'.join(keywords)
+    return df[df['Objeto'].str.contains(pattern, case=False, na=False)]
 
 # URL da página para scraping
 url = 'https://www.infrasa.gov.br/licitacoes/'
 
+# Headers para a requisição HTTP
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
 # Fazer a requisição HTTP
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 
 if response.status_code == 200:
     site = BeautifulSoup(response.text, 'html.parser')
+    table = site.find('table', {'id': 'table_1'})
 
-    # Localizar a tabela
-    tabela = site.find('table', attrs={'id': 'table_1'})
+    # Extraindo cabeçalhos das colunas
+    headers = [header.get_text(strip=True) for header in table.find_all('th')]
 
-    if tabela:
-        # Extrair as linhas da tabela
-        rows = tabela.findAll('tr')
+    # Extraindo dados das linhas
+    data = []
+    for row in table.find('tbody').find_all('tr'):
+        cols = row.find_all('td')
+        data.append([col.get_text(strip=True) for col in cols])
 
-        # Extrair dados de cada linha
-        data_bruta = []
-        for row in rows:
-            cells = row.findAll('td')
-            if cells:  # Verificar se a linha possui células
-                cells = [cell.get_text(strip=True) for cell in cells]
-                data_bruta.append(cells)
+    # Criando o DataFrame
+    df_completo = pd.DataFrame(data, columns=headers)
+    df_completo = df_completo.map(remove_special_characters)
+    
+    # Salvando em um arquivo Excel
+    save_to_excel(df_completo, 'licitacoes_infrasa_completo.xlsx')
 
-        # Determinar o número de colunas com base na primeira linha
-        num_columns = len(data_bruta[0]) if data_bruta else 0
+    # Filtrar por Situação
+    situacao_filtro = "em andamento"
+    df_filtrado = df_completo[df_completo['Situação'] == situacao_filtro]
+    df_filtrado = keywords_filter(df_filtrado, keywords)
 
-        # Definir nomes das colunas com base no número de colunas presentes
-        columns = ['Data da Publicação', 'Título', 'Nº da licitação', 'Modalidade', 'DataAbertura', 'Objetivo', 'Situação', 'Empresa', 'Data'][:num_columns]
-
-        # Criar DataFrame completo
-        try:
-            df_completa = pd.DataFrame(data_bruta, columns=columns)
-        
-            save_to_excel(df_completa, 'Licitações_infrasa_completa.xlsx')
-            print("Dados completos salvos e formatados em Licitações_infrasa_completa.xlsx")
-
-        except ValueError as e:
-            print(f"Erro ao criar DataFrame: {e}")
-
-        df_tratada = df_completa.drop(['Data da Publicação', 'Título', 'Nº da licitação', 'Modalidade', ])
-    else:
-        print("Tabela não encontrada na página.")
+    save_to_excel(df_filtrado, 'licitacoes_infra_tratada.xlsx')
+    
 else:
     print(f"Falha ao buscar a página. Código de status: {response.status_code}")
