400ecd5f819c5acfc62653203932e61aa45800f3
João Leonardo Filgueiras Rodrigues <74377581+joaole@users.noreply.github.com>
2024-07-16 09:09:25 -0300

    Update ws_porto_são_francisco_do_sul.py

--- ws_porto_são_francisco_do_sul.py
+++ ws_porto_são_francisco_do_sul.py
@@ -1,9 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-from date_data_processing import process_data
 from remove_special_characters import remove_special_characters
 from save_to_excel import save_to_excel
+
+keywords = [
+    "evtea", "projeto basico", "executivo", "conceitual", "economico", "meio ambiente", "consultoria", "engenharia consultiva",
+    "estruturacao de projetos", "estudos de viabilidade", "conceitual de projetos de infraestrutura", "estudo de pre-viabilidade",
+    "estudo de viabilidade tecnico economico ambiental", "projetos conceituais", "projeto conceitual", "projetos basicos",
+    "projeto basico", "projetos executivos", "projeto executivo", "gerenciamento de obras", "gerenciamento de obra",
+    "supervisao e acompanhamento de obras", "planejamento estrategico", "plano de negocios", "plano de negocio", "planos mestres",
+    "plano mestre", "planos de investimentos", "plano de investimento", "plano de gestao e monitoriamento", "planos diretores",
+    "plano diretor", "estudos ambientais", "estudo ambiental", "gestao continuada", "planos e programas", "plano e programa",
+    "estudo de impacto ambiental e relatorio de impacto ambiental eia/rima", "estudo de impacto ambiental", "relatorio de impacto ambiental",
+    "eia", "rima", "avaliacoes regulatorias", "avaliacao relatoria", "materiais de audiencias publicas", "material de audiencia publica",
+    "pareceres tecnicos", "parecer tecnico", "avaliacao de adequacao de atendimentos", "avaliacoes operacionais", "avaliacao operacional",
+    "avaliacao de capacidade", "avaliacao de desempenho operacional", "planos de expansao", "plano de expansao", "monitoriamento da eficiencia",
+    "simulacoes operacionais", "simulacao operacional", "macro e microssimulacao de transporte", "macro", "macro simulacao",
+    "microssimulacao", "microssimulacao operacional", "estudos economicos", "analises conjunturais", "analise conjuntural",
+    "avaliacao de mercado", "avaliacoes de mercados", "previsoes e cenarios", "previsoes", "cenarios", "estruturacao de projetos",
+    "plano de investimentos", "plano de gestao e monitoriamento", "planos diretores", "plano diretor", "estudos ambientais",
+    "estudo ambiental", "gestao continuada", "planos e programas", "plano e programa", "eia", "rima", "licitatório",  'estudos tecnicos'
+]
+
+def keywords_filter(df, keywords):
+    pattern = '|'.join(keywords)
+    return df[df['Objetivo'].str.contains(pattern, case=False, na=False)]
 
 # URL da página para scraping
 url = 'https://portosaofrancisco.com.br/licitacoes/'
@@ -21,24 +43,24 @@ if response.status_code == 200:
     containers = site.findAll('div', attrs={'class': 'col-sm-12 dados'})
     
     data_completa = []
-    data_tratada = []
     
     for container in containers:
         data = container.findAll('div', attrs={'class':'col-sm-12'})
-        row = [remove_special_characters([d.get_text(strip=True)])[0] for d in data]
+        row = [d.get_text(strip=True) for d in data]
         link_edital = container.find('a', href=True)['href'] if container.find('a', href=True) else ''
         row.append(link_edital)
         data_completa.append(row)
 
     max_columns = max(len(row) for row in data_completa)
-    colunas = ['Modalidade', 'Edital', 'DataAbertura', 'Objetivo', 'Link Edital'] + [f'Column {i+1}' for i in range(5, max_columns)]
+    colunas = ['Modalidade', 'Edital', 'DataAbertura', 'Objetivo', '', '', 'Link']
     
     for row in data_completa:
         while len(row) < max_columns:
             row.append('')
 
     df_completa = pd.DataFrame(data_completa, columns=colunas)
-    df_tratada = pd.DataFrame(data_tratada, columns=colunas).apply(process_data)
+    df_completa = df_completa.applymap(remove_special_characters)
+    df_tratada = keywords_filter(df_completa, keywords)
     
     save_to_excel(df_completa, 'Licitacoes_porto_são_Francisco_COMPLETA.xlsx')
     save_to_excel(df_tratada, 'Licitacoes_porto_são_Francisco_TRATADA.xlsx')
